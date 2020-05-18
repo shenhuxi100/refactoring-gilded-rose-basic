382d4ec3bb5fc47b9e2bc2fb8fa0c4b357be73d0
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-18 22:00:15 +0800

    Change item[i] to Item object

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -9,60 +9,61 @@ class GildedRose {
 
     public void update_quality() {
         for (int i = 0; i < commodityItems.length; i++) {
-            if (commodityItems[i].commodityName.equals("Aged Brie")
-                    || commodityItems[i].commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                handleQualityLessThan50(i);
+            CommodityItem item = commodityItems[i];
+            if (item.commodityName.equals("Aged Brie")
+                    || item.commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                handleQualityLessThan50(item);
             } else {
-                commodityItemQualityMinusOne(i);
+                commodityItemQualityMinusOne(item);
             }
 
-            if (!commodityItems[i].commodityName.equals("Sulfuras, Hand of Ragnaros")) {
-                commodityItems[i].sellIn = commodityItems[i].sellIn - 1;
+            if (!item.commodityName.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            handleSellInLessThanZero(i);
+            handleSellInLessThanZero(item);
         }
     }
 
-    private void handleQualityLessThan50(int i) {
-        if (commodityItems[i].quality < 50) {
-            commodityItems[i].quality = commodityItems[i].quality + 1;
+    private void handleQualityLessThan50(CommodityItem item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
 
-            if (commodityItems[i].commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (commodityItems[i].sellIn < 11) {
-                    commodityItemQualityPlusOne(i);
+            if (item.commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                    commodityItemQualityPlusOne(item);
                 }
 
-                if (commodityItems[i].sellIn < 6) {
-                    commodityItemQualityPlusOne(i);
+                if (item.sellIn < 6) {
+                    commodityItemQualityPlusOne(item);
                 }
             }
         }
     }
 
-    private void handleSellInLessThanZero(int i) {
-        if (commodityItems[i].sellIn < 0) {
-            if (commodityItems[i].commodityName.equals("Aged Brie")) {
-                commodityItemQualityPlusOne(i);
+    private void handleSellInLessThanZero(CommodityItem item) {
+        if (item.sellIn < 0) {
+            if (item.commodityName.equals("Aged Brie")) {
+                commodityItemQualityPlusOne(item);
             } else {
-                if (commodityItems[i].commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    commodityItems[i].quality = 0;
+                if (item.commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    item.quality = 0;
                 } else {
-                    commodityItemQualityMinusOne(i);
+                    commodityItemQualityMinusOne(item);
                 }
             }
         }
     }
 
-    private void commodityItemQualityPlusOne(int i) {
-        if (commodityItems[i].quality < 50) {
-            commodityItems[i].quality = commodityItems[i].quality + 1;
+    private void commodityItemQualityPlusOne(CommodityItem item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 
-    private void commodityItemQualityMinusOne(int i) {
-        if (commodityItems[i].quality > 0 && !commodityItems[i].commodityName.equals("Sulfuras, Hand of Ragnaros")) {
-            commodityItems[i].quality = commodityItems[i].quality - 1;
+    private void commodityItemQualityMinusOne(CommodityItem item) {
+        if (item.quality > 0 && !item.commodityName.equals("Sulfuras, Hand of Ragnaros")) {
+            item.quality = item.quality - 1;
         }
     }
 }
