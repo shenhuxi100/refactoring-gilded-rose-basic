7afbe6403627f779351e905a0925d7ba7400ebe0
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-18 22:10:44 +0800

    move item update related operations to Item class

--- src/main/java/cn/xpbootcamp/gilded_rose/CommodityItem.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/CommodityItem.java
@@ -18,4 +18,61 @@ public class CommodityItem {
     public String toString() {
         return this.commodityName + ", " + this.sellIn + ", " + this.quality;
     }
+
+    void commodityItemQualityMinusOne() {
+        if (quality > 0 && !commodityName.equals("Sulfuras, Hand of Ragnaros")) {
+            quality = quality - 1;
+        }
+    }
+
+    void commodityItemQualityPlusOne() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+    }
+
+    void handleSellInLessThanZero() {
+        if (sellIn < 0) {
+            if (commodityName.equals("Aged Brie")) {
+                commodityItemQualityPlusOne();
+            } else {
+                if (commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    quality = 0;
+                } else {
+                    commodityItemQualityMinusOne();
+                }
+            }
+        }
+    }
+
+    void handleQualityLessThan50() {
+        if (quality < 50) {
+            quality = quality + 1;
+
+            if (commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (sellIn < 11) {
+                    commodityItemQualityPlusOne();
+                }
+
+                if (sellIn < 6) {
+                    commodityItemQualityPlusOne();
+                }
+            }
+        }
+    }
+
+    void updateCommodityItemQuality() {
+        if (commodityName.equals("Aged Brie")
+                || commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            handleQualityLessThan50();
+        } else {
+            commodityItemQualityMinusOne();
+        }
+
+        if (!commodityName.equals("Sulfuras, Hand of Ragnaros")) {
+            sellIn = sellIn - 1;
+        }
+
+        handleSellInLessThanZero();
+    }
 }
--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -10,60 +10,7 @@ class GildedRose {
     public void update_quality() {
         for (int i = 0; i < commodityItems.length; i++) {
             CommodityItem item = commodityItems[i];
-            if (item.commodityName.equals("Aged Brie")
-                    || item.commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                handleQualityLessThan50(item);
-            } else {
-                commodityItemQualityMinusOne(item);
-            }
-
-            if (!item.commodityName.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            handleSellInLessThanZero(item);
-        }
-    }
-
-    private void handleQualityLessThan50(CommodityItem item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.sellIn < 11) {
-                    commodityItemQualityPlusOne(item);
-                }
-
-                if (item.sellIn < 6) {
-                    commodityItemQualityPlusOne(item);
-                }
-            }
-        }
-    }
-
-    private void handleSellInLessThanZero(CommodityItem item) {
-        if (item.sellIn < 0) {
-            if (item.commodityName.equals("Aged Brie")) {
-                commodityItemQualityPlusOne(item);
-            } else {
-                if (item.commodityName.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    item.quality = 0;
-                } else {
-                    commodityItemQualityMinusOne(item);
-                }
-            }
-        }
-    }
-
-    private void commodityItemQualityPlusOne(CommodityItem item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void commodityItemQualityMinusOne(CommodityItem item) {
-        if (item.quality > 0 && !item.commodityName.equals("Sulfuras, Hand of Ragnaros")) {
-            item.quality = item.quality - 1;
+            item.updateCommodityItemQuality();
         }
     }
 }
